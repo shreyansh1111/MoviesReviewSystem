e8bd62cedaf8569c4309c670c9e01ea3739c0408
shreyansh1111 <85796572+shreyansh1111@users.noreply.github.com>
2024-01-29 12:04:39 +0530

    Add files via upload

---
+++ movies/movies/src/main/java/com/shreyansh/movies/MovieService.java
@@ -0,0 +1,22 @@
+package com.shreyansh.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MoviesRepository moviesRepository;
+
+    public List<Movie> allMovies(){
+        return moviesRepository.findAll();
+
+    }
+    public Optional<Movie> singeMovie(String imdbId){
+        return moviesRepository.findMovieByImdbId(imdbId);
+    }
+}
